90a886495163a49924664a82548432e07d3c388a
Bartosz Renski <bartek.renifer@gmail.com>
2015-06-11 22:02:23 +0200

    Added RichTechtBox control and it's label

--- BoringApplication/MainForm.Designer.cs
+++ BoringApplication/MainForm.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ConsoleRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.ConsoleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConsoleRichTextBox
+            // 
+            this.ConsoleRichTextBox.Location = new System.Drawing.Point(12, 150);
+            this.ConsoleRichTextBox.Name = "ConsoleRichTextBox";
+            this.ConsoleRichTextBox.Size = new System.Drawing.Size(673, 271);
+            this.ConsoleRichTextBox.TabIndex = 0;
+            this.ConsoleRichTextBox.Text = "";
+            // 
+            // ConsoleLabel
+            // 
+            this.ConsoleLabel.AutoSize = true;
+            this.ConsoleLabel.Location = new System.Drawing.Point(12, 118);
+            this.ConsoleLabel.Name = "ConsoleLabel";
+            this.ConsoleLabel.Size = new System.Drawing.Size(128, 17);
+            this.ConsoleLabel.TabIndex = 1;
+            this.ConsoleLabel.Text = "Tu będzie konsola:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 433);
+            this.Controls.Add(this.ConsoleLabel);
+            this.Controls.Add(this.ConsoleRichTextBox);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox ConsoleRichTextBox;
+        private System.Windows.Forms.Label ConsoleLabel;
     }
 }
 
